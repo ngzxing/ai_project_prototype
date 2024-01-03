dd9a3587df50a5b381df76d63ca699b3b788d3ae
NG ZI XING <92634733+ngzxing@users.noreply.github.com>
2024-01-04 00:18:30 +0800

    Add files via upload

--- pages/Food_Manager.py
+++ pages/Food_Manager.py
@@ -3,65 +3,71 @@ import numpy as np
 
 st.write("# Food Manager")
 
-tab1, tab2 = st.tabs(["#### Ingredients List", "#### Order List"])
+if "diet" not in st.session_state:
 
-with tab1:
-    st.error("There are some **food expired**, kinldy check the following list")
-    with st.expander("See Details"):
-        st.markdown("""
-        
-        The list is showing the details of ingredients inside the refrigerator:
+    st.warning("Please Go To Diet Planner, before go back this page")
 
-        > `ingredient` is the name of ingredient
-        
-        > `status` is showing whether the food is expired
-        
-        > `target_weight` is showing the expected ingredients amount should have in refrigerator
-        
-        > `weight` is the actual weight of ingredients inside the refrigerator
+else:
 
-        > `Expired Date` is the expired date of the ingredients
-        
-                    """)
-        
-    st.table(st.session_state["ingredients"][["Ingredient", "Target Weight", "Weight", "Status", "Expired Date"]])
-        
-
-with tab2:
-
-    def replace(i, order_status):
-        st.session_state.ingredients.loc[i, "Order Status"] = order_status
-
-    def show_container(i):
+    tab1, tab2 = st.tabs(["#### Ingredients List", "#### Order List"])
     
-        if st.session_state.ingredients.loc[i, "Order Status"] == "Sufficient":
-            return
+    with tab1:
+        st.error("There are some **food expired**, kinldy check the following list")
+        with st.expander("See Details"):
+            st.markdown("""
             
-        with st.container():
+            The list is showing the details of ingredients inside the refrigerator:
+    
+            > `ingredient` is the name of ingredient
             
-            columns = st.columns(3)
-            columns[0].write( f":red[{st.session_state.ingredients.loc[i, 'Ingredient']}]")
-            columns[0].divider()
-            columns[1].write( st.session_state.ingredients.loc[i, "Order Status"])
-            columns[1].divider()
-
+            > `status` is showing whether the food is expired
             
-            if st.session_state.ingredients.loc[i, "Order Status"] == "Ordering":
+            > `target_weight` is showing the expected ingredients amount should have in refrigerator
+            
+            > `weight` is the actual weight of ingredients inside the refrigerator
+    
+            > `Expired Date` is the expired date of the ingredients
+            
+                        """)
+            
+        st.table(st.session_state["ingredients"][["Ingredient", "Target Weight", "Weight", "Status", "Expired Date"]])
+            
+    
+    with tab2:
+    
+        def replace(i, order_status):
+            st.session_state.ingredients.loc[i, "Order Status"] = order_status
+    
+        def show_container(i):
+        
+            if st.session_state.ingredients.loc[i, "Order Status"] == "Sufficient":
+                return
                 
-                columns[2].button(f"Cancel Order", key = f"Cancel Order{i}", 
-                                  on_click = lambda : replace(i, "Not Ordering"), type = "primary", use_container_width=True )
+            with st.container():
+                
+                columns = st.columns(3)
+                columns[0].write( f":red[{st.session_state.ingredients.loc[i, 'Ingredient']}]")
+                columns[0].divider()
+                columns[1].write( st.session_state.ingredients.loc[i, "Order Status"])
+                columns[1].divider()
+    
+                
+                if st.session_state.ingredients.loc[i, "Order Status"] == "Ordering":
                     
-            elif st.session_state.ingredients.loc[i, "Order Status"] == "Not Ordering":
-                columns[2].button(f"Order", key = f"Order{i}", 
-                                  on_click = lambda : replace(i, "Ordering"), type = "secondary", use_container_width=True )
-
-
-                
-            
-
-    columns = st.columns(3)
-    columns[0].write("### Ingredient")
-    columns[1].write("### Status")
+                    columns[2].button(f"Cancel Order", key = f"Cancel Order{i}", 
+                                      on_click = lambda : replace(i, "Not Ordering"), type = "primary", use_container_width=True )
+                        
+                elif st.session_state.ingredients.loc[i, "Order Status"] == "Not Ordering":
+                    columns[2].button(f"Order", key = f"Order{i}", 
+                                      on_click = lambda : replace(i, "Ordering"), type = "secondary", use_container_width=True )
     
-    for i in range( st.session_state.ingredients.shape[0] ):    
-        show_container(i)
+    
+                    
+                
+    
+        columns = st.columns(3)
+        columns[0].write("### Ingredient")
+        columns[1].write("### Status")
+        
+        for i in range( st.session_state.ingredients.shape[0] ):    
+            show_container(i)
--- pages/My_Diet.py
+++ pages/My_Diet.py
@@ -2,17 +2,22 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.write("# Your Goals: ")
-st.write("Here is the goals your diet planner try to achieve in one month")
-st.write("- " + "\n- ".join(st.session_state.goals) )
+if "diet" not in st.session_state:
 
-st.write("# Your Diet Planning")
-st.write("*repeat this 7 days routine diet in one month*")
-tabs = st.tabs( [ "###### " + key for key in st.session_state.diet.keys() ] )
-
-for tab, day in zip(tabs, st.session_state.diet):
-
-    with tab:
-        plan = pd.DataFrame(st.session_state.diet[day]).T
-        st.table(plan)
-        st.write(f"###### Total Calories of that Day: `{plan['Calories'].sum()}` ")
+    st.warning("Please Go To Diet Planner, before go back this page")
+else:
+    
+    st.write("# Your Goals: ")
+    st.write("Here is the goals your diet planner try to achieve in one month")
+    st.write("- " + "\n- ".join(st.session_state.goals) )
+    
+    st.write("# Your Diet Planning")
+    st.write("*repeat this 7 days routine diet in one month*")
+    tabs = st.tabs( [ "###### " + key for key in st.session_state.diet.keys() ] )
+    
+    for tab, day in zip(tabs, st.session_state.diet):
+    
+        with tab:
+            plan = pd.DataFrame(st.session_state.diet[day]).T
+            st.table(plan)
+            st.write(f"###### Total Calories of that Day: `{plan['Calories'].sum()}` ")
